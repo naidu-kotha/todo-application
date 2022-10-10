65331d17702a04e8d6c2a4bcf59547dbece042d8
naidu-kotha <srinivasnaidu1605@gmail.com>
2022-10-10 10:13:17 +0000

    adds update todo API

--- app.js
+++ app.js
@@ -49,16 +49,14 @@ const hasPriority = (requestQuery) => {
 };
 
 
-
-
 // Get All ToDo items with Different Query Parameters
 app.get("/todos/", async(request, response) => {
-    let getTodosQuery = "";
+    let getAllTodosQuery = "";
     const { search_q = "", priority, status } = request.query;
 
     switch (true) {
         case hasPriorityAndStatus(request.query):
-            getTodosQuery = `
+            getAllTodosQuery = `
             SELECT
               *
             FROM
@@ -69,7 +67,7 @@ app.get("/todos/", async(request, response) => {
               AND priority = '${priority}';`;
             break;
         case hasStatus(request.query):
-            getTodosQuery = `
+            getAllTodosQuery = `
             SELECT
               *
             FROM
@@ -79,7 +77,7 @@ app.get("/todos/", async(request, response) => {
               AND status = '${status}';`;
             break;
         case hasPriority(request.query):
-            getTodosQuery = `
+            getAllTodosQuery = `
             SELECT
               *
             FROM
@@ -89,7 +87,7 @@ app.get("/todos/", async(request, response) => {
               AND priority = '${priority}';`;
             break;
         default:
-            getTodosQuery = `
+            getAllTodosQuery = `
             SELECT
               *
             FROM
@@ -99,9 +97,109 @@ app.get("/todos/", async(request, response) => {
             break;
     };
 
-    todos = await db.all(getTodosQuery);
+    todos = await db.all(getAllTodosQuery);
 
     response.send(todos);
+});
+
+
+// Get ToDo item by ToDO Id
+app.get("/todos/:todoId/", async(request, response) => {
+    const { todoId } = request.params;
+
+    const getTodoQuery = `
+    SELECT
+      *
+    FROM
+      todo
+    WHERE
+      id = ${todoId};`;
+    
+    const todo = await db.get(getTodoQuery);
+
+    response.send(todo);
+});
+
+
+// Create ToDo item
+app.post("/todos/", async(request, response) => {
+    const { id, todo, status, priority } = request.body;
+
+    const addTodoQuery = `
+    INSERT INTO
+      todo (id, todo, status, priority)
+    VALUES
+      ('${id}', '${todo}', '${status}', '${priority}');`;
+
+    await db.run(addTodoQuery);
+
+    response.send("Todo Successfully Added");
+});
+
+
+// Update Todo Item by ToDO Id
+app.put("/todos/:todoId/", async(request, response) => {
+    const { todoId } = request.params;
+
+    let updateTodoQuery = "";
+
+    let updatedColumn = "";
+
+    const requestBody = request.body;
+
+    switch (true) {
+        case requestBody.status !== undefined:
+
+            const { status } = request.body;
+
+            updatedColumn = "Status";
+
+            updateTodoQuery = `
+            UPDATE
+              todo
+            SET
+              status = '${status}'
+            WHERE
+              id = ${ todoId };`;
+
+            break;
+
+        case requestBody.priority !== undefined:
+
+            const { priority } = request.body;
+
+            updatedColumn = "Priority";
+
+            updateTodoQuery = `
+            UPDATE
+              todo
+            SET
+              priority = '${priority}'
+            WHERE
+              id = ${ todoId };`;
+
+            break;
+
+        case requestBody.todo !== undefined:
+
+            const { todo } = request.body;
+
+            updatedColumn = "Todo";
+
+            updateTodoQuery = `
+            UPDATE
+              todo
+            SET
+              todo = '${ todo }'
+            WHERE
+              id = ${ todoId };`;
+
+            break;
+    };
+
+    await db.run(updateTodoQuery);
+
+    response.send(`${updatedColumn} Updated`);
 });
 
 
